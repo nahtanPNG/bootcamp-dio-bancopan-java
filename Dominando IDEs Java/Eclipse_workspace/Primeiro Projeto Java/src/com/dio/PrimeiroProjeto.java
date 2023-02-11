bb9ef4e2acce91b3edb21347428fd4c284fdd179
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-11 15:00:45 -0300

    Primeiro Projeto com Eclipse

---
+++ Dominando IDEs Java/Eclipse_workspace/Primeiro Projeto Java/src/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Para criar essa estrutura você pode digitar MAIN, dar um ctrl + espaço e selecionar a opção que queira
+		int a = 2;
+		int b = 2;
+		System.out.println("Hello World!" + (a+b)); 
+	}
+
+}
