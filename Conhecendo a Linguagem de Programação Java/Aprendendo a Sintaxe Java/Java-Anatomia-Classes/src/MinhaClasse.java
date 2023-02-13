151322d7c2cc1bac387aedf9faefe595e565beec
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-13 19:45:02 -0300

    Variaveis e Metodos

--- Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Anatomia-Classes/src/MinhaClasse.java
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Anatomia-Classes/src/MinhaClasse.java
@@ -1,6 +1,15 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String primeiroNome = "Nathan";
+        String segundoNome = "Ferreira";
+
+        // VARIAVEL E METODO()
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
