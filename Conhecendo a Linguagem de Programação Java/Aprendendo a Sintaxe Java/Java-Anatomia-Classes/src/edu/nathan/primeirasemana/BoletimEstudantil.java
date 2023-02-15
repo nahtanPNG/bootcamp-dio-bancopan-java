34a38d51758221fc8819b793128047d776ffb55d
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-15 19:16:31 -0300

    Configurando packages

---
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Anatomia-Classes/src/edu/nathan/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+package edu.nathan.primeirasemana;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if(mediaFinal<6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
