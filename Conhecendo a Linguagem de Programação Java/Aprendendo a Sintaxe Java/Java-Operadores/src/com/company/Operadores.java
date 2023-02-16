044611c75eb0119854e79ad71f735782861df3b7
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-16 19:26:06 -0300

    Operadores

---
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Operadores/src/com/company/Operadores.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+	    String nomeCompleto = "Linguagem " + "Java";
+        System.out.println(nomeCompleto);
+    }
+}
