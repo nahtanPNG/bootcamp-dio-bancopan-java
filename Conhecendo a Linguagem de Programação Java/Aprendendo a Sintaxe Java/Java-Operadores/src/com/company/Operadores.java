0e1c304f4691188220423d742374f6c3601dbe79
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-16 19:36:47 -0300

    Operadores Unários

--- Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Operadores/src/com/company/Operadores.java
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Operadores/src/com/company/Operadores.java
@@ -3,8 +3,26 @@ package com.company;
 public class Operadores {
 
     public static void main(String[] args) {
-        
+        //CONCATENAÇÃO
+
 	    String nomeCompleto = "Linguagem " + "Java";
         System.out.println(nomeCompleto);
+
+        //Unários
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = + numero; //Para converter em forma de positivo precisamos fazer uma multiplicação
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        //Incremento de números
+
+        
+
     }
 }
