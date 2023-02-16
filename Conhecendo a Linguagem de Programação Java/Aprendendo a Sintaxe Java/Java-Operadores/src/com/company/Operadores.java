55e2e9ecc90d58d98bff10e46d71a6f82c865f96
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-16 20:04:37 -0300

    Operadores finalizados

--- Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Operadores/src/com/company/Operadores.java
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Operadores/src/com/company/Operadores.java
@@ -8,7 +8,7 @@ public class Operadores {
 	    String nomeCompleto = "Linguagem " + "Java";
         System.out.println(nomeCompleto);
 
-        //Unários
+        /*Unários
         int numero = 5;
 
         numero = - numero;
@@ -21,8 +21,67 @@ public class Operadores {
         System.out.println(numero);
 
         //Incremento de números
+         */
 
-        
+        int numero = 5;
 
+        numero++; //igual numero = numero + 1
+
+        System.out.println( -- numero);
+        System.out.println(numero);
+
+        //Anulando boolean
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel; //reatribuindo, passando de true para false
+
+        System.out.println(variavel);
+
+        //Ternário
+
+        int a, b;
+        a = 5;
+        b = 6;
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        */
+
+        System.out.println(resultado);
+
+        //Relacionais
+
+        int numero1 = 1;
+        int numero2 = 2;
+        boolean simNao = numero1 == numero2;
+        String nomeUm = "Nathan";
+        String nomeDois = new String("Gleyson");
+
+        System.out.println(nomeUm.equals(nomeDois)); //Equals compara conteudos dos objetos
+
+        System.out.println("NumeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("NumeroUm é Diferente a numeroDois? " + simNao);
+
+        //lógicas
+
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
     }
 }
