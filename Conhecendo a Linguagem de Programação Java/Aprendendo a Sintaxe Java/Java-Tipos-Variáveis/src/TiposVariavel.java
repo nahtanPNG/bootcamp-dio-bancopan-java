3f9c925332527b4ea61a7e3724d62034f0aa74f3
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-16 19:07:59 -0300

    Tipos de variaveis

--- Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Tipos-Variáveis/src/TiposVariavel.java
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Tipos-Variáveis/src/TiposVariavel.java
@@ -1,5 +1,12 @@
 public class TiposVariavel {
     public static void main(String[] args) {
-        
+        double salarioMinimo = 2500.33;
+
+        byte idade = 123;
+        short ano = 2023;
+        int cep = 21070333; //se começar com zero, talvez tenha usar outro tipo - java não armazena o zero
+        long cpf = 103485476292L; //se começar com zero, talvez tenha usar outro tipo - tem que por L no final
+        float pi = 3.14F; //Tem que colocar o F no final
+        double salario = 1275.22;
     }
 }
