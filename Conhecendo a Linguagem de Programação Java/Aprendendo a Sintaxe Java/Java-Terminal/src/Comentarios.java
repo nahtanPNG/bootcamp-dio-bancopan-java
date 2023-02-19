c86ea5e81f25a55e913f669964718a703583811c
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-19 13:13:50 -0300

    Docs html

--- Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Terminal/src/Comentarios.java
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Terminal/src/Comentarios.java
@@ -14,4 +14,23 @@ public class Comentarios {
          * Que incrível !!!
          */
     }
+
+    /*
+    FORMA ERRADA DE UTILIZAR OS COMENTÁRIOS
+
+     * Este método foi elaborado as pressas
+     * por isso eu abreviei o nome das variáveis
+     * mas olha, ele tem a finalidade de somar ou  multiplicar
+     * dois números
+     * */
+    public int somaMultiplica(int n, int x, String m) {
+        int r = 0; // r é igual ao resultado
+        if (m == "M") { // M= multiplicação
+            r = n * x;
+        } else {
+            // se não soma mesmo
+            r = n + x;
+        }
+        return r;
+    }
 }
