94090b29b2a87190761c7cbe9fbb433941f39822
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-19 13:06:09 -0300

    Java Doc

---
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Terminal/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+        /* Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhadod
+         * quando necessário
+         */
+        /**
+         * Estas duas estrelinhas acima
+         * é para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!
+         */
+    }
+}
