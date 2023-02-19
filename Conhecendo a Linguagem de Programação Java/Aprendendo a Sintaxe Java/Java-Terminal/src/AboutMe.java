58693de005d61a1c096c18df1c2ac7e814be59be
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-02-19 14:05:07 -0300

    Scanner

--- Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Terminal/src/AboutMe.java
+++ Conhecendo a Linguagem de Programação Java/Aprendendo a Sintaxe Java/Java-Terminal/src/AboutMe.java
@@ -1,7 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
         //os argumentos começam com indice 0
-        String nome = args [0];
+        /* String nome = args [0];
         String sobrenome = args [1];
         int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
         double altura = Double.valueOf(args[3]);
@@ -9,6 +12,27 @@ public class AboutMe {
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
+         */
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next(); //Next retorna string
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "M ");
     }
 }
 
