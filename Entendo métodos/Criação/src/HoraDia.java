6f804ec5fe136160ba632fe0e1d93d6ca2359e0c
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-03-17 19:07:43 -0300

    Exercicios Métodos finalizados

--- Entendo métodos/Criação/src/HoraDia.java
+++ Entendo métodos/Criação/src/HoraDia.java
@@ -1,2 +1,13 @@
 public class HoraDia {
+
+    public static void mensagemHorário(int hora){
+        if(hora > 6 & hora <12){
+            System.out.println("Bom dia!");
+        } else if(hora >= 12 & hora < 18){
+            System.out.println("Boa tarde!");
+        } else{
+            System.out.println("Boa noite!");
+        }
+    }
+
 }
--- Entendo métodos/Criação/src/main.java
+++ Entendo métodos/Criação/src/main.java
@@ -2,13 +2,17 @@ public class main {
     public static void main(String[] args) {
 
         //Operações
+        System.out.println("Calculadora");
         operaçõesBasicas.somar(3, 4);
         operaçõesBasicas.subtração(3, 4);
         operaçõesBasicas.multiplicar(3, 4);
         operaçõesBasicas.dividir(3, 4);
 
-
-
+        //Hora do dia
+        System.out.println("Mensagem");
+        HoraDia.mensagemHorário(4);
+        HoraDia.mensagemHorário(13);
+        HoraDia.mensagemHorário(7);
 
 
     }
